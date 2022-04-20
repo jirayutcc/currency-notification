2750b6f2e52dfbe00b7cd540a967e96f714334c9
jirayutcc <jirayut_ssww@hotmail.com>
2022-04-20 15:36:08 +0700

    init project

---
+++ src/main/java/com/currency/currency/notification/CurrencyNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.currency.currency.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyNotificationApplication.class, args);
+	}
+
+}
