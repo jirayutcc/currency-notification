405253311a5a8ca6e59867f7d08e8de4c63ca546
jirayutcc <jirayut_ssww@hotmail.com>
2022-05-24 20:49:56 +0700

    rename main

--- src/test/java/com/currency/currencynotification/CurrencyNotificationApplicationTests.java
+++ src/test/java/com/currency/currencynotification/CurrencyNotificationApplicationTests.java
@@ -1,4 +1,4 @@
-package com.currency.currency.notification;
+package com.currency.currencynotification;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
