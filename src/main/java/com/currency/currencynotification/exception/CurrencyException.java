aea07f32ad46f49fe8f653bbf05ee696be937c01
jirayutcc <jirayut_ssww@hotmail.com>
2022-05-26 22:38:57 +0700

    added exception

---
+++ src/main/java/com/currency/currencynotification/exception/CurrencyException.java
@@ -0,0 +1,9 @@
+package com.currency.currencynotification.exception;
+
+public class CurrencyException extends RuntimeException {
+
+    public CurrencyException(String msg) {
+        super(msg);
+    }
+
+}
