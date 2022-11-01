d29b28fa9535949380b1508c89b573dd07ca345c
jirayut cc <jirayut_ssww@hotmail.com>
2022-11-01 22:43:31 +0700

    added http exrption

---
+++ src/main/java/com/currency/currencynotification/exception/HttpException.java
@@ -0,0 +1,9 @@
+package com.currency.currencynotification.exception;
+
+public class HttpException extends RuntimeException {
+
+    public HttpException(String msg) {
+        super(msg);
+    }
+
+}
