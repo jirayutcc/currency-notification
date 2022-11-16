1a3e53b5713cb55c002de6765c40d5caa1a7ff03
jirayut cc <jirayut_ssww@hotmail.com>
2022-11-16 13:53:24 +0700

    added getWithParameters

--- src/main/java/com/currency/currencynotification/adapter/HttpClientAdapter.java
+++ src/main/java/com/currency/currencynotification/adapter/HttpClientAdapter.java
@@ -2,6 +2,8 @@ package com.currency.currencynotification.adapter;
 
 import com.currency.currencynotification.exception.CurrencyException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.utils.URIBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +12,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+@Slf4j
 @Component
 public class HttpClientAdapter {
 
@@ -34,6 +37,37 @@ public class HttpClientAdapter {
                     request,
                     HttpResponse.BodyHandlers.ofString()
             );
+
+            if (response.statusCode() != HttpStatus.OK.value()) {
+                throw new CurrencyException(errMessage);
+            }
+
+            return objectMapper.readValue(response.body(), clazz);
+
+        } catch (Exception ex) {
+            throw new CurrencyException(ex.getMessage());
+        }
+    }
+
+    public <T> T getWithParameters(String url, Class<T> clazz, String currencyType, String errMessage) throws CurrencyException {
+
+        try {
+
+            URI uri = new URIBuilder(url)
+                    .addParameter("base", currencyType)
+                    .build();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(uri)
+                    .GET()
+                    .build();
+            log.info("[GET] request: {}", request);
+
+            HttpResponse<String> response = httpClient.send(
+                    request,
+                    HttpResponse.BodyHandlers.ofString()
+            );
+            log.info("[GET] response: {}, body: {}", response, response.body());
 
             if (response.statusCode() != HttpStatus.OK.value()) {
                 throw new CurrencyException(errMessage);
