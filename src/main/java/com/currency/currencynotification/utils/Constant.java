3e3692de85e7db2bdc144975f89a6fc3b78ed3df
jirayut cc <jirayut_ssww@hotmail.com>
2022-11-16 13:55:02 +0700

    updated utils

--- src/main/java/com/currency/currencynotification/utils/Constant.java
+++ src/main/java/com/currency/currencynotification/utils/Constant.java
@@ -4,4 +4,10 @@ public class Constant {
 
     public static final String BTC_ERROR_MESSAGE = "Could not retrieve BTC rate";
 
+    public static class CurrencyType {
+        public static final String BTC = "BTC";
+        public static final String XRP = "XRP";
+        public static final String DOGE = "DOGE";
+        public static final String USDC = "USDC";
+    }
 }
---
+++ src/main/java/com/currency/currencynotification/utils/ErrorMapping.java
@@ -0,0 +1,6 @@
+package com.currency.currencynotification.utils;
+
+public class ErrorMapping {
+
+    public static final String CODE9999 = "9999";
+}
