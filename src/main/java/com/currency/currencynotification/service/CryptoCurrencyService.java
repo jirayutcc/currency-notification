25585e87c747c1b4c6b542f93e902989b307c284
jirayut cc <jirayut_ssww@hotmail.com>
2022-11-22 16:52:20 +0700

    aaded crypto currency service

---
+++ src/main/java/com/currency/currencynotification/service/CryptoCurrencyService.java
@@ -0,0 +1,31 @@
+package com.currency.currencynotification.service;
+
+import com.currency.currencynotification.adapter.HttpClientAdapter;
+import com.currency.currencynotification.exception.CurrencyException;
+import com.currency.currencynotification.dto.CryptoCurrencyList;
+import com.currency.currencynotification.properties.CurrencyProperties;
+import com.currency.currencynotification.utils.Constant;
+import org.springframework.stereotype.Service;
+
+import static com.currency.currencynotification.utils.Constant.BTC_ERROR_MESSAGE;
+
+@Service
+public class CryptoCurrencyService {
+
+    private final HttpClientAdapter httpClientAdapter;
+    private final CurrencyProperties currencyProperties;
+
+    public CryptoCurrencyService(HttpClientAdapter httpClientAdapter, CurrencyProperties currencyProperties) {
+        this.httpClientAdapter = httpClientAdapter;
+        this.currencyProperties = currencyProperties;
+    }
+
+    public CryptoCurrencyList getBitcoinCurrency() throws CurrencyException {
+        return httpClientAdapter.getWithParameters(
+                currencyProperties.getCurrencyUrl(),
+                CryptoCurrencyList.class,
+                Constant.CurrencyType.BTC,
+                BTC_ERROR_MESSAGE
+        );
+    }
+}
